befd5ea0b94f34419d613cabcd10147b98b48fad
Mulla Javed <168650100+MullaZee@users.noreply.github.com>
2025-06-23 22:33:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 API_ID = int(os.environ.get("API_ID", "523894"))
 API_HASH = os.environ.get("API_HASH", "e15ad5314295f")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "683894700450:AAGtmIuSJgmjbkbi3vs")
-CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "<b>{file_caption}</b>\n\n<b>JOIN 💎 : @M2LINKS</b>")
-ADMINS = os.environ.get("ADMINS", "6167872503")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "<b>{file_caption}</b>\n\n<b>JOIN 💎 : @MoviZenXFlix</b>")
+ADMINS = os.environ.get("ADMINS", "7439670062")
