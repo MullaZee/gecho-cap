95747b840e20968c2505da691a4d4d43e7f18f89
Saikat <84172414+saikatwtf@users.noreply.github.com>
2025-07-06 01:44:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 API_ID = int(os.environ.get("API_ID", "523894"))
 API_HASH = os.environ.get("API_HASH", "e15ad5314295f")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "<b>{file_caption}</b>\n\n<b>JOIN 💎 : @MoviZenXFlix</b>\n\n━━━━━━━━━━━━━━━━━━━━━━\n<b>For Loots & Steal Deals Join @CheapieDeals</b>")
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "<b>{file_caption}</b>\n\n<b>JOIN 💎 : @MoviZenXFlix</b>\n\n<b>For Loots & Steal Deals Join @CheapieDeals</b>")
 ADMINS = os.environ.get("ADMINS", "7439670062")
